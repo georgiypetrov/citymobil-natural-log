1d2841fcdbc57fd4640e20a2ade83a72c4031c4a
allerria <wtf496@gmail.com>
2020-04-26 07:55:27 +0300

    update train data in hyperparam search.

--- hyperparam_tuning.py
+++ hyperparam_tuning.py
@@ -12,10 +12,8 @@ from xgboost import XGBRegressor
 
 from utils import mean_absolute_percentage_error, WeightedRegressor, preprocess
 
-df_train = pd.read_csv('data/train_with_arrived_error_q80.csv', parse_dates=['OrderedDate'])
+df_train = pd.read_csv('data/train_with_arrived_error_q90.csv', parse_dates=['OrderedDate'])
 df_val = pd.read_csv('data/validation.csv', parse_dates=['OrderedDate'])
-
-df_train = df_train.sample(250000, random_state=1337)
 
 X_train = preprocess(df_train)
 y_train = df_train['RTA']
